72d843ee6aef6ab45c6108a31a8273e4607d4f31
Gustavo Todeschini <gutodeschinii@outlook.com>
2025-03-09 16:08:26 -0300

    Aprendendo a estrutura de um projeto react

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,11 @@
+import { MyButton } from './styles'
+
+function Button() {
+    return (
+        <div>
+            <MyButton>Clica em mim</MyButton>    
+        </div>
+    )
+}
+
+export default Button
